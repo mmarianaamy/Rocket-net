e603a3bd7c80d82db81a49d6db283bf56feab72e
Mariana Amy <63118971+mmarianaamy@users.noreply.github.com>
2024-05-14 16:30:28 -0600

    impacto

--- src/Components/Beneficios.jsx
+++ src/Components/Beneficios.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import background from '../Assets/background1.png'
+import people from "../Assets/beneficios.png"
 
 export default function Beneficios() {
   return (
-    <div style={{backgroundImage: `url(${background})`}}>
-      Beneficios
+    <div id="Beneficios" style={{backgroundImage: `url(${background})`}}>
+      <div class="beneficiostop">
+        <div>
+          <p class="beneficiosheading">Impacto que aportarás al participar</p>
+          <br></br>
+          <p><b>Desafio 01 - Potencia la estrategia de growth enfocada en promociones y precios</b></p>
+          <p>Agilizar el proceso de toma de pedido para simplificar este proceso al cliente, y así ayudarlo a aumentar sus ingresos al facilitarle la visualización y el entendimiento de nuestras promociones, lo cual, al mismo tiempo, nos ayudará a aumentar nuestras ventas. </p>
+          <br></br>
+          <p><b>Desafío 02 - Securing the Digital Nest: identifica vulnerabilidades</b></p>
+          <p>Poner a prueba los controles de seguridad implementados en la arquitectura de la solución y encontrar vulnerabilidades que puedan ser explotadas por agentes externos en cualquier rango de expertise y su posible solución. Esperamos que la mejora continua de la seguridad genere mayor confianza con nuestros clientes.</p>
+          <br></br>
+          <p><b>Desafío 03 - Hackea el churn: anticipando e identificando la posible baja de clientes</b></p>
+          <p>Identificar clientes con una alta probabilidad de darse de baja en el futuro próximo para diseñar estrategias de acompañamiento con la finalidad de evitar la baja y lograr redirigir recursos para invertir en clientes con mayor potencial de crecimiento.</p>
+        </div>
+        <img src={people}></img>
+      </div>
     </div>
   )
 }
